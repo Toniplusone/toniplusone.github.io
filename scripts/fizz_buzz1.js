08ab76e6fcb895ce91be3a9702f55abf9f260f14
Toni Dotson <42526430+Toniplusone@users.noreply.github.com>
2021-10-03 11:35:09 -0400

    Add files via upload

---
+++ scripts/fizz_buzz1.js
@@ -0,0 +1,50 @@
+function getData() {
+  let firstName = document.getElementById('first_name').value;
+  let lastName = document.getElementById('last_name').value;
+  let middleInitial = document.getElementById('middle_initial').value;
+        
+  if (middleInitial) {
+     document.getElementById("greeting").textContent = "Welcome to Toni's Business Solutions, " + firstName + " " + middleInitial + ". " + lastName + "!";
+   } else {
+     document.getElementById("greeting").textContent = "Welcome to Toni's Business Solutions, " + firstName + " " + lastName + "!";
+   }
+  
+  businessSol();
+}
+
+function businessSol() {
+  for (let i = 1; i <= 140; i++) {
+    var word = '';
+    
+    if (i % 3 === 0) {
+      word = word.concat('HELP');
+    } 
+    
+    if (i % 5 === 0) {
+      word = word.concat('SOLUTIONS!');
+    }
+    
+    if (word === '') {
+      word = 'Yea';
+    }
+      
+      
+    
+   
+    var newIten = document.createElement('li');
+    
+    
+    var newText = document.createTextNode(i + ": " + word);
+    
+    
+    newItem.appendChild(newText);
+    
+    
+    var newPosition = document.getElementsByTagName('ul')[0];
+    
+    
+    newPosition.appendChild(newItem);
+    
+  }
+  
+}
